6112f3af535e28a5ecf39f6d9531b57bb29837a1
William <williquisti@gmail.com>
2023-05-20 15:39:59 -0500

    validation of entered point was made

--- src/components/Point.jsx
+++ src/components/Point.jsx
@@ -6,8 +6,38 @@ function Point({text}) {
   let {input, setInput} = useContext(InputContext)
 
   function  handleClick(){
-    setInput(input += text)
+    let lastChar = input[input.length-1] // last digited character   
+
+    if(!(lastChar === '.' || lastChar === '/' || lastChar === 'X' || lastChar === '-' || lastChar === '+')){
+
+      let conStart = 0
+      for(let i=input.length-1;i>= 0; i--){
+        if(input[i] === '.'){
+          conStart++        
+        }
+      }
+      if(conStart === 0){
+        setInput(input += text)        
+      }
+
+      let conOperator = 0
+      let j= input.length-1
+      console.log(j)
+      while(input[j] !== '/' && input[j] !== 'X' && input[j] !== '-' && input[j] !== '+' && j>=0){
+        if(input[j] === '.'){
+          conOperator++
+          console.log(conOperator)        
+        }
+        j--
+        console.log('j',j)
+      }
+      if(conOperator === 0){
+        setInput(input += text)      
+      }
+            
+    }    
   }
+  
   return (
     <>
       <div  className='col-6 h-15 p-3' onClick={handleClick}>
